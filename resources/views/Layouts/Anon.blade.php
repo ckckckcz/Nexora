1d3fd9f43121b303f37334a905a612ffc10ad9e2
ckckckcz <rriovld@gmail.com>
2025-04-17 14:17:30 +0700

    refactor: update landing page layout and remove welcome view

---
+++ resources/views/Layouts/Anon.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Pemuda Kerja</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
+        <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+        <!-- Styles / Scripts -->
+        @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
+            @vite(['resources/css/app.css', 'resources/js/app.js'])
+        @else
+        @endif
+    </head>
+    <body>
+        <h1 class="text-purple-500 font-bold">Halo Kong</h1>
+    </body>
+</html>
