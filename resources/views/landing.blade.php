8fbc90d1ef3006dfdb27e5e09156ae72d711f27b
ckckckcz <rriovld@gmail.com>
2025-04-19 20:58:59 +0700

    feat: add 'Why Us' section to landing page and create corresponding view component

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -4,4 +4,6 @@
     @include('components.ui.anon.hero')
     <!-- Company Logo Section -->
     @include('components.ui.anon.company-logo')
+    <!-- Why Us Section -->
+    @include('components.ui.anon.whyus')
 @endsection
