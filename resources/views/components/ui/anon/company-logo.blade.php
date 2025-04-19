8af9e130de9b3198a15c040757e234c8f6754ef7
ckckckcz <rriovld@gmail.com>
2025-04-19 12:11:05 +0700

    feat: add company logo section to landing page

---
+++ resources/views/components/ui/anon/company-logo.blade.php
@@ -0,0 +1,34 @@
+<section class="py-8 md:py-12 bg-gray-50">
+    <div class="container mx-auto px-4">
+        <div class="flex flex-wrap justify-center items-center gap-8 md:gap-16">
+            <img
+                src="{{ asset('images/placeholder.svg') }}"
+                alt="Google"
+                class="opacity-50 hover:opacity-75 transition-opacity"
+                width="120"
+                height="40"
+            />
+            <img
+                src="{{ asset('images/placeholder.svg') }}"
+                alt="Facebook"
+                class="opacity-50 hover:opacity-75 transition-opacity"
+                width="120"
+                height="40"
+            />
+            <img
+                src="{{ asset('images/placeholder.svg') }}"
+                alt="Airbnb"
+                class="opacity-50 hover:opacity-75 transition-opacity"
+                width="120"
+                height="40"
+            />
+            <img
+                src="{{ asset('images/placeholder.svg') }}"
+                alt="Slack"
+                class="opacity-50 hover:opacity-75 transition-opacity"
+                width="120"
+                height="40"
+            />
+        </div>
+    </div>
+</section>
--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -2,4 +2,6 @@
 @section('content')
     <!-- Hero Section -->
     @include('components.ui.anon.hero')
+    <!-- Company Logo Section -->
+    @include('components.ui.anon.company-logo')
 @endsection
