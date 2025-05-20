e3a6516d57e220e8437f478400f81a9af4e43fab
Cakra Wangsa M.A.W <ckrawangsa@gmail.com>
2025-05-20 11:46:31 +0700

    feat(migrations): add criteria table

---
+++ database/migrations/2025_05_20_044422_create_kriteria_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('kriteria', function (Blueprint $table) {
+            $table->id('id_kriteria')->autoIncrement();
+            $table->string('nama_kriteria', 100);
+            $table->float('bobot');
+            $table->enum('tipe', ['benefit', 'cost']);
+            $table->text('keterangan');
+            $table->timestamps(); // Added timestamps to match other tables
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('kriteria');
+    }
+};
