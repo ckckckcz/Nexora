8666fb25d081e1f2fb4abeaeccd74ae7a4cef1aa
ckckckcz <rriovld@gmail.com>
2025-05-21 13:20:46 +0700

    feat(education): replace SVG icon with logo image for improved visual representation

--- resources/views/components/ui/user/content/education.blade.php
+++ resources/views/components/ui/user/content/education.blade.php
@@ -7,15 +7,8 @@
     </div>
     <div class="mt-3">
         <div class="flex items-start">
-            <div class="h-10 w-10 rounded bg-gray-100 flex items-center justify-center flex-shrink-0">
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-gray-500" fill="none" viewBox="0 0 24 24"
-                    stroke="currentColor">
-                    <path d="M12 14l9-5-9-5-9 5 9 5z" />
-                    <path
-                        d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z" />
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                        d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222" />
-                </svg>
+            <div class="h-10 w-10 rounded flex items-center justify-center flex-shrink-0">
+                <img src="https://upload.wikimedia.org/wikipedia/id/4/4a/Logo_Politeknik_Negeri_Malang.png" alt="">
             </div>
             <div class="ml-3">
                 <h4 class="text-sm font-semibold text-gray-900">State Polytechnic of Malang</h4>
