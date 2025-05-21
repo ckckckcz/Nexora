dc01ce9b0af8235a1d3bcc5623a5f1741d9c5b4b
ckckckcz <rriovld@gmail.com>
2025-05-21 11:16:19 +0700

    fix(view): correct rotation angle of background overlay in company logo section

--- resources/views/components/ui/anon/company-logo.blade.php
+++ resources/views/components/ui/anon/company-logo.blade.php
@@ -1,5 +1,5 @@
 <section class="relative lg:mt-24 mt-0">
-    <div class="absolute -inset-x-2 -inset-y-2 right-1 bg-[#ddfc7994] rotate-2 -z-30"></div>
+    <div class="absolute -inset-x-2 -inset-y-2 right-1 bg-[#ddfc7994] rotate-1 -z-30"></div>
     <div class="absolute inset-0 bg-wine -z-20"></div>
     <section class="relative py-4 text-center lg:py-8 bg-blue-900 overflow-hidden">
         <div id="scroll-container-1" class="flex lg:gap-12 gap-8 whitespace-nowrap"></div>
