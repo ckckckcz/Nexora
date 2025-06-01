367f32d771e62c3f0944951a8715300db1f44b26
Cakra Wangsa M.A.W <ckrawangsa@gmail.com>
2025-06-01 14:24:51 +0700

    fix(migrations): improvement of recommendation results

--- database/migrations/2025_05_21_020530_create_hasil_rekomendasi_table.php
+++ database/migrations/2025_05_21_020530_create_hasil_rekomendasi_table.php
@@ -4,32 +4,28 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateHasilRekomendasiTable extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
+    public function up()
     {
         Schema::create('hasil_rekomendasi', function (Blueprint $table) {
-            $table->id('id_hasil_rekomendasi')->autoIncrement();
+            $table->id('id_hasil_rekomendasi');
+
             $table->unsignedBigInteger('id_mahasiswa');
-            $table->unsignedBigInteger('id_dosen');
-            $table->decimal('nilai_akhir', 8, 2);
+            $table->foreign('id_mahasiswa')->references('id_user')->on('users')->onDelete('cascade');
+
+            $table->unsignedBigInteger('id_lowongan');
+            $table->foreign('id_lowongan')->references('id_lowongan')->on('lowongan_magang')->onDelete('cascade');
+
+            $table->float('nilai_akhir');
             $table->integer('ranking');
+            $table->boolean('rekomendasi_dosen')->default(false);
             $table->timestamps();
-
-            $table->foreign('id_mahasiswa')->references('id_mahasiswa')->on('mahasiswa')->onDelete('cascade');
-            $table->foreign('id_dosen')->references('id_dosen')->on('dosen')->onDelete('cascade');
-
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('hasil_rekomendasi');
     }
-};
+}
