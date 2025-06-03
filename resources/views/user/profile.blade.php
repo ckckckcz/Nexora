403756fd322e87e12e7d504d47421dc55c3fb923
ckckckcz <rriovld@gmail.com>
2025-06-03 09:45:59 +0700

    fix(view) : comment out activity tab button in profile view

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -75,10 +75,10 @@
                         class="tab-button border-blue-900 text-blue-900 whitespace-nowrap py-4 px-1 border-b-2 font-medium text-sm active">
                         Profile
                     </button>
-                    <button data-tab="activity"
+                    {{-- <button data-tab="activity"
                         class="tab-button border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 whitespace-nowrap py-4 px-1 border-b-2 font-medium text-sm">
                         Log Aktivitas
-                    </button>
+                    </button> --}}
                 </nav>
             </div>
 
